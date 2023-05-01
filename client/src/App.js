e44cf7dc4bf2d4b95dba5041672bc62ca949fd65
574515 <574515c0d1ng@gmail.com>
2023-05-01 04:51:50 +0200

    Added router and basic routing for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home/>,
+	},
+	{
+		path: "/register",
+		element: <Register/>
+	},
+	{
+		path: "/login",
+		element: <Login/>
+	}
+]);
+
 function App() {
-  return (
-    <div>Hello, world!</div>
-  );
+	return (
+			<RouterProvider router={router}/>
+	);
 }
 
 export default App;
