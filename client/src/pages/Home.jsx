511d5be685e19f86bace6ba10d1fc5486c9ecfde
574515 <574515c0d1ng@gmail.com>
2023-05-02 20:15:45 +0200

    Added dummy data to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 
 const Home = () => {
+	const notes = [
+		{
+			id: 1,
+			title: "Test 1",
+			desc: "Test 1 Desc"
+		},
+		{
+			id: 2,
+			title: "Test 2",
+			desc: "Test 2 Desc"
+		},
+		{
+			id: 3,
+			title: "Test 3",
+			desc: "Test 3 Desc"
+		}
+	]
 	return (
-			<div>Home</div>
+			<div className="home">
+				<div className="notes">
+					{notes.map((note) => (
+							<div className="post" key={note.id}>
+								<h1>{note.title}</h1>
+								<p>{note.desc}</p>
+							</div>
+					))}
+				</div>
+			</div>
 	);
 };
 
