6075a1963cc3f86df0fe8c602fa0531bbdecbf82
574515 <574515c0d1ng@gmail.com>
2023-05-05 14:57:44 +0200

    Added routes, body parser and cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import notesRoutes from "./routes/notes.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(express.json());
+app.use(cors());
+
+app.use("/api/notes", notesRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(8800, () => {
 	console.log("Listening on port 8800");
