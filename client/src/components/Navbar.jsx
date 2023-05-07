d4bafa90d51bab50b995175156e19cdcd9ddeb19
574515 <574515c0d1ng@gmail.com>
2023-05-07 15:32:35 +0200

    Minor changes in navbar, removed not needed links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,15 +14,8 @@ const Navbar = () => {
 						<img src={Logo} alt=""/>
 					</div>
 					<div className="links">
-						<Link className="link" to="/login">
-							<h6>LOGIN</h6>
-						</Link>
-						<Link className="link" to="/register">
-							<h6>REGISTER</h6>
-						</Link>
 						<span>{currentUser?.username}</span>
 						{currentUser ? <span onClick={logout}>Logout</span> : <Link className="link" to="/login">Login</Link>}
-						<span className="write">Write</span>
 					</div>
 				</div>
 			</div>
