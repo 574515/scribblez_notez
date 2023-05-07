5bd284bfdb0e76713182b3149ec88fd855ed925a
574515 <574515c0d1ng@gmail.com>
2023-05-07 15:33:31 +0200

    Added globals

---
+++ client/src/globals.js
@@ -0,0 +1,6 @@
+class InputClassStyles {
+	errorInput = "errorInput my-3 form-control form-control-sm";
+	validInput = "validInput my-3 form-control form-control-sm";
+}
+
+module.exports = new InputClassStyles();
