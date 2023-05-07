a57be71eba964bcf55565059635e8f2a86c13624
574515 <574515c0d1ng@gmail.com>
2023-05-07 17:26:57 +0200

    Added different styling for password and other fields

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,6 +1,8 @@
 class InputClassStyles {
-	errorInput = "errorInput my-3 form-control form-control-sm";
-	validInput = "validInput my-3 form-control form-control-sm";
+	errorInput = "errorInput my-2 form-control form-control-sm";
+	validInput = "validInput my-2 form-control form-control-sm";
+	errorPasswordInput = "errorInput my-2 w-75 form-control form-control-sm";
+	validPasswordInput = "validInput my-2 w-75 form-control form-control-sm";
 }
 
 module.exports = new InputClassStyles();
