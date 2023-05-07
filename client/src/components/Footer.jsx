5b348c8e21adfad6fc209995bf273c1d648893d1
574515 <574515c0d1ng@gmail.com>
2023-05-07 22:42:56 +0200

    Made whole footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import Logo from '../assets/img/logo.png';
+import Facebook from '../assets/img/facebook.svg'
+import Git from '../assets/img/git.svg';
+import Instagram from '../assets/img/instagram.svg';
+import LinkedIn from '../assets/img/linkedin.svg';
+import Twitter from '../assets/img/twitter.svg';
 
 const Footer = () => {
 	return (
-			<footer>
-				<img src={Logo} alt="Logo"/>
-				<span>Web Programming (C) 2023</span>
+			<footer className="text-center text-white">
+				<div className="p-2">
+					<section className="my-2">
+						<a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://facebook.com" target="_blank" role="button" data-mdb-ripple-color="dark" rel="noreferrer">
+							<img src={Facebook} alt=""/>
+						</a>
+						<a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://github.com/574515" target="_blank" role="button" data-mdb-ripple-color="dark" rel="noreferrer">
+							<img src={Git} alt=""/>
+						</a>
+						<a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://instagram.com" target="_blank" role="button" data-mdb-ripple-color="dark" rel="noreferrer">
+							<img src={Instagram} alt=""/>
+						</a>
+						<a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://linkedin.com" target="_blank" role="button" data-mdb-ripple-color="dark" rel="noreferrer">
+							<img src={LinkedIn} alt=""/>
+						</a>
+						<a className="btn btn-link btn-floating btn-lg text-dark m-1" href="https://twitter.com" target="_blank" role="button" data-mdb-ripple-color="dark" rel="noreferrer">
+							<img src={Twitter} alt=""/>
+						</a>
+					</section>
+				</div>
+				<div className="text-center text-dark">&copy; 2023 Web Programming</div>
 			</footer>
 	);
 };
