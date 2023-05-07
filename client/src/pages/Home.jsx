dc4fc497949c6968d43f2d1f8590db48d161176f
574515 <574515c0d1ng@gmail.com>
2023-05-07 15:36:00 +0200

    Refactored home to use Note component, work needed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, {useCallback, useContext, useEffect, useMemo, useState} from 'react';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import Note from "../components/Note";
+import {AuthContext} from "../context/authContext";
+import AddNote from "../components/AddNote";
 
 const Home = () => {
-	const notes = [
-		{
-			id: 1,
-			title: "Test 1",
-			desc: "Test 1 Desc"
-		},
-		{
-			id: 2,
-			title: "Test 2",
-			desc: "Test 2 Desc"
-		},
-		{
-			id: 3,
-			title: "Test 3",
-			desc: "Test 3 Desc"
+	const navigate = useNavigate();
+
+	const [notes, setNotes] = useState([]);
+	const {currentUser} = useContext(AuthContext);
+	let params = useMemo(() => {
+		return {currentUserId: null}
+	}, []);
+
+	const fetchData = useCallback(async () => {
+		try {
+			const res = await axios.get("http://localhost:8800/api/notes", {params});
+			setNotes(res.data);
+		} catch (err) {
+			console.log(err);
 		}
-	]
+	}, [params]);
+
+	useEffect(() => {
+		if (currentUser === null) {
+			navigate("/login");
+		} else {
+			params.currentUserId = currentUser.id;
+		}
+		fetchData().then();
+	}, [fetchData, currentUser, navigate, params]);
+
 	return (
 			<div className="home">
-				<div className="notes">
-					{notes.map((note) => (
-							<div className="post" key={note.id}>
-								<h1>{note.title}</h1>
-								<p>{note.desc}</p>
-							</div>
-					))}
+				<AddNote fetchData={fetchData}/>
+				<div className="notes row g-5">
+					{notes.map((note) => <Note noteData={note} fetchData={fetchData} key={note.id}/>)}
 				</div>
 			</div>
 	);
