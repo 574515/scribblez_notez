2c88218127f621f33831bf3c6855afe8eed3fcaa
574515 <574515c0d1ng@gmail.com>
2023-05-09 10:39:18 +0200

    Minor changes

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 import {toISOStringWithOffset} from "../helpers.js";
 
 export const getNotes = (req, res) => {
-	if (req.query.currentUserId) {
+	if (req.query.currentUserUsername) {
 		const query = "SELECT * FROM notes WHERE sn_user = ? ORDER BY id DESC";
-		db.query(query, [req.query.currentUserId], (err, data) => {
+		db.query(query, [req.query.currentUserUsername], (err, data) => {
 			if (err) return req.send(err);
 			return res.status(200).json(data);
 		});
@@ -17,17 +17,16 @@ export const getNote = (req, res) => {
 }
 
 export const addNote = (req, res) => {
-	const insertQuery = "INSERT INTO notes(`title`, `body`, `create_date`, `sn_user`) VALUES (?)";
-	console.log(req.body.currentUser.id);
+	const insertQuery = "INSERT INTO notes(title, body, create_date, sn_user) VALUES (?)";
 	const insertValues = [
 		req.body.inputs.title,
 		req.body.inputs.body,
 		toISOStringWithOffset(new Date()),
-		req.body.currentUser.id
+		req.body.currentUserUsername
 	];
 	db.query(insertQuery, [insertValues], (err, data) => {
 		if (err) return res.json(err);
-		return res.status(200).json("User has been created!");
+		return res.status(200).json("Note has been created!");
 	});
 }
 
@@ -38,7 +37,7 @@ export const deleteNote = (req, res) => {
 		if (err) return res.status(403).json("Token is not valid!");
 		const noteId = req.params.id;
 		const query = "DELETE FROM notes WHERE id = ? AND sn_user = ?";
-		db.query(query, [noteId, userInfo.id], (err, data) => {
+		db.query(query, [noteId, userInfo.username], (err, data) => {
 			if (err) return res.status(403).json("You can delete only your notes!");
 			return res.status(200).json("Note has been deleted.");
 		});
@@ -46,5 +45,30 @@ export const deleteNote = (req, res) => {
 }
 
 export const updateNote = (req, res) => {
-	res.json("From controller!");
+	const query = "UPDATE notes SET title=(?), body=(?), update_date=(?) WHERE notes.id=(?)"
+	const values = [
+		req.body.inputs.title,
+		req.body.inputs.body,
+		toISOStringWithOffset(new Date()),
+		req.body.id
+	];
+	db.query(query, values, (err, data) => {
+		if (err) return res.json(err);
+		return res.status(200).json("Updated!");
+	});
+}
+
+export const updateVisibility = (req, res) => {
+	db.query(`UPDATE notes SET is_public = ${req.body.is_public} WHERE id = ${req.body.id}`, (err, data) => {
+		if (err) return res.json(err);
+		return res.status(200).json(`Note has been made ${req.body.is_public ? "public" : "private"}!`);
+	});
+}
+
+export const getPublicNotes = (req, res) => {
+	const query = "SELECT * FROM notes WHERE is_public = 1 ORDER BY id DESC";
+	db.query(query, (err, data) => {
+		if (err) return req.send(err);
+		return res.status(200).json(data);
+	});
 }
