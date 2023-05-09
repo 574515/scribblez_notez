c3e55c68de5667be0a7e3148c75e168c0fa0a173
574515 <574515c0d1ng@gmail.com>
2023-05-09 10:47:41 +0200

    Made Explore functional

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import {AuthContext} from "../context/authContext";
+import Note from "../components/Note";
 
 const Explore = () => {
+	const navigate = useNavigate();
+
+	const [notes, setNotes] = useState([]);
+	const {currentUser} = useContext(AuthContext);
+
+
+	const fetchData = useCallback(async () => {
+		try {
+			const res = await axios.get("/api/notes/public");
+			setNotes(res.data);
+		} catch (err) {
+			console.log(err);
+		}
+	}, [notes]);
+
+
+	useEffect(() => {
+		fetchData().then();
+	}, [fetchData, currentUser, navigate]);
 
 	return (
 			<div className="home">
+				<div className="notes row row-cols-4 g-2 mt-3">
+					{notes.map((note) => <div className="col singleNote" key={note.id}><Note isPublic={true} key={note.id} noteData={note} fetchData={fetchData}/></div>)}
+				</div>
 			</div>
 	);
 };
