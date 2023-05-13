7228e800e8f7ba71153bb80b7f53c3cba8d251dd
574515 <574515c0d1ng@gmail.com>
2023-05-14 01:10:05 +0200

    Added routes for all usernames and some profile updates

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,12 @@
 import express from "express";
-import {getUser, getUserNotes} from "../controllers/users.js";
+import {getUser, getUserNotes, updateUserProfile, updateIsAnonymous, removeProfilePicture, getAllUsernames} from "../controllers/users.js";
 
 const router = express.Router();
 
+router.get("/usernames", getAllUsernames);
 router.get("/:username", getUser);
 router.get("/:username/notes/:isCurrentUser", getUserNotes);
+router.put("/:username", updateUserProfile);
+router.patch("/:username", updateIsAnonymous);
+router.patch("/:username/image", removeProfilePicture);
 export default router;
