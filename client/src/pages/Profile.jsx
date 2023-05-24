a4021d568b9cd2a83c9b6f50f76a801f5c89b48e
574515 <574515c0d1ng@gmail.com>
2023-05-24 14:24:29 +0200

    Bootstrap optimization, added count for public notes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,13 +15,15 @@ const Profile = () => {
 	const isCurrentUser = params.username === currentUser?.username;
 	const endpoints = useMemo(() => {
 		return [
-			`/api/users/${params.username}`,
+			`api/users/${params.username}`,
 			`api/users/${params.username}/notes/${isCurrentUser}`,
-			`api/notes/count/${params.username}/${isCurrentUser}`
+			`api/notes/count/${params.username}/${isCurrentUser}`,
+			`api/notes/count/public/${params.username}`
 		];
 	}, [isCurrentUser, params.username]);
 	const [notes, setNotes] = useState([]);
 	const [notesCount, setNotesCount] = useState(0);
+	const [publicNotesCount, setPublicNotesCount] = useState(0);
 	const [showTopBtn, setShowTopBtn] = useState(false);
 	const [user, setUser] = useState([]);
 
@@ -30,26 +32,27 @@ const Profile = () => {
 				endpoints.map((endpoint) =>
 						axios
 								.get(endpoint)))
-				.then(([user, notes, count]) => {
+				.then(([user, notes, count, publicCount]) => {
 					setUser(user.data[0]);
 					setNotes(notes.data);
 					setNotesCount(count.data);
+					setPublicNotesCount(publicCount.data);
 				});
 	}, [endpoints]);
 
 	useEffect(() => {
 		getData();
 		window.addEventListener('scroll', () => setShowTopBtn(window.scrollY > 150));
-	}, [currentUser, getData, navigate, params]);
+	}, [currentUser, getData, navigate, params, publicNotesCount]);
 
 	return (
-			<div className="notes row row-cols-4 mt-3 g-2">
-				<div className={(user.is_anonymous && user?.id !== currentUser?.id) ? "col-6 mx-auto" : "col-3"}>
-					{user.length !== 0 && <UserInfo notesCount={notesCount} notes={notes} params={params} key={user.id} userData={user} getData={getData}/>}
+			<div className="notes row mt-3 g-2">
+				<div className={(user.is_anonymous && user?.id !== currentUser?.id) ? "col-8 mx-auto" : publicNotesCount > 0 ? "d-none d-xl-block col-3" : "col-8 mx-auto"}>
+					{user.length !== 0 && <UserInfo notesCount={notesCount} publicNotesCount={publicNotesCount} notes={notes} params={params} key={user.id} userData={user} getData={getData}/>}
 				</div>
-				<div className={(user.is_anonymous && user?.id !== currentUser?.id) ? "d-none" : "col-9 row g-2"}>
-					{(user.is_anonymous && user?.id !== currentUser?.id) ? "" : notes.map((note) => <div className="col-4 singleNote" key={note.id}>
-						<Note key={note.id} noteData={note} fetchData={getData}/>
+				<div className={(user.is_anonymous && user?.id !== currentUser?.id) ? "d-none" : "col-12 col-xl-9 row"}>
+					{(user.is_anonymous && user?.id !== currentUser?.id) ? "" : notes.map((note) => <div className="col-12 col-md-6 col-lg-4 my-2 singleNote" key={note.id}>
+						<Note key={note.id} noteData={note} getData={getData()}/>
 					</div>)}
 				</div>
 				{showTopBtn && <div className="top-to-btm">
